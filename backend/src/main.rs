37eea27d4d7794b710dbad47db1b61e647e4a524
Daniel Steman <daniel-steman@live.nl>
2023-08-18 14:08:14 +0200

    fix import

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Parser};
 use std::fs;
 use warp::Filter;
 
@@ -10,9 +10,9 @@ async fn main() {
         let markdown_content =
             fs::read_to_string(&file_path).unwrap_or_else(|_| String::from("Post not found"));
 
-        let parser = pulldown_cmark::Parser::new(&markdown_content);
+        let parser = Parser::new(&markdown_content);
         let mut html_output = String::new();
-        pulldown_cmark::html::push_html(&mut html_output, parser);
+        html::push_html(&mut html_output, parser);
 
         warp::reply::html(html_output)
     });
