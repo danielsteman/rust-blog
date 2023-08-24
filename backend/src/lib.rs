3491e0aa9f38989841b3ca94ff9fc1d6c5223aeb
danielsteman <daniel-steman@live.nl>
2023-08-24 09:22:14 +0200

    added util fn

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,18 @@
+use regex::Regex;
+
+pub fn find_words_between_colons(text: &str) -> Vec<String> {
+    let re = Regex::new(r":\s*([^:\s]+)\s*:?").unwrap();
+    let mut words = Vec::new();
+
+    for capture in re.captures_iter(text) {
+        if let Some(word) = capture.get(1) {
+            words.push(word.as_str().to_string());
+        }
+    }
+
+    words
+}
+
 pub fn find_emojis(text: &str) -> &str {
     text
 }
